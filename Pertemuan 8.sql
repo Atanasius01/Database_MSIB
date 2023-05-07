fd647e2890935a77838fc0086625c0e2d9d57219
Atanasius01 <atanasius256@gmail.com>
2023-05-07 14:16:08 +0700

    Update tugas Pertemuan 8

---
+++ Pertemuan 8.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+MariaDB [dbtoko1]> CREATE PROCEDURE InputPesanan(id int, tanggal date, total int, pelanggan_id int)
+    -> BEGIN
+    -> INSERT INTO pesanan (id, tanggal, total, pelanggan_id) VALUES (id, tanggal, total, pelanggan_id);
+    -> END$$
+ DELIMITER ;
+MariaDB [dbtoko1]> CALL InputPesanan('3','2023-07-05','5000000','2');
+
+CREATE TRIGGER list_pembayaran AFTER INSERT ON pesanan
+FOR EACH ROW	
+BEGIN
+INSERT INTO pesanan SET
+id = new.id, no_kuitansi= new.no_kuitansi, tanggal= new.tanggal, ke = new.ke, pesanan_id = new.pesanan_id;
+END
+
+CREATE TRIGGER pembayaran_after_update
+AFTER UPDATE ON pembayaran
+FOR EACH ROW
+BEGIN
+  IF NEW.status_pembayaran = 'Lunas' THEN
+    UPDATE pembayaran SET status_pembayaran = 'Lunas'
+    WHERE id = NEW.id;
+  END IF;
+END;
